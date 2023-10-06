41f27e87501e9533cd99adbb40e8640c4ae73136
rafael lucilio <rafael.alves149@etec.sp.gov.br>
2023-10-06 17:31:55 -0300

    Aula API

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,9 +4,34 @@ import BannerMovies from '../../components/bannerFilmes';
 import CardMovies from '../../components/cardFilmes';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchbar';
+import React,{useState,useEffect} from 'react'
 import Filmes from '../../data/movies'
 
-export default function App() {
+export default function App() {   
+  const[movies, setMovies] = useState([]);
+
+
+  useEffect(()=>{   
+      async function buscarFilmes(){
+
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=31fd33a81283a95b801e3d29b5e76446&language=pt%20BR')
+        const data = await response.json();
+        console.log(data.results);
+        setMovies(data.results)
+
+      }
+
+      buscarFilmes();
+
+
+
+
+
+  },[])
+
+
+
+
   return (
     <View style={styles.container}>
      <Header></Header>
